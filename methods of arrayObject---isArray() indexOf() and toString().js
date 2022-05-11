aeb28946d4ad51b848491bffee3168c4a4d0a8d1
Dmitriy <groont@list.ru>
2022-05-11 14:46:56 +0300

    fixed methods of arrayObject---isArray() indexOf() and toString()

--- methods of arrayObject---isArray() indexOf() and toString().js
+++ methods of arrayObject---isArray() indexOf() and toString().js
@@ -2,8 +2,10 @@
 function blackAndWhite(arr) {
     let a;
     !Array.isArray(arr) ? a = "It's a fake array" :
-        ((arr.indexOf(13) >= 0) && (arr.indexOf(5) >= 0)) ? 
+        ((arr.indexOf(13) >= 0) && (arr.indexOf(5) >= 0)) ?
         a = "It's a black array" :
         a = "It's a white array";
     return a;
 }
+
+console.log(blackAndWhite([5, 12]));
