ee3598719943c3b47877dbfa06ab2e47e0f08486
Dmitriy <groont@list.ru>
2022-05-17 23:14:26 +0300

    add String cleaning

---
+++ String cleaning.js
@@ -0,0 +1,5 @@
+//String cleaning
+function stringClean(s) {
+    return s.replace(/[0-9]/gi, "");
+}
+console.log(stringClean('"My \"me3ssy\" d8ata issues2! Will1 th4ey ever, e3ver be3 so0lved?'));
