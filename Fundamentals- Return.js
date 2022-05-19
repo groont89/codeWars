971cd7ee4ca7787ac6e260e4cf612d2637b388be
Dmitriy <groont@list.ru>
2022-05-19 11:10:41 +0300

    add Fundamentals: Return

---
+++ Fundamentals- Return.js
@@ -0,0 +1,26 @@
+//Fundamentals: Return
+function add(a, b) {
+    return a + b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
+
+function exponent(a, b) {
+    return Math.pow(a, b);
+}
+
+function subt(a, b) {
+    return a - b;
+}
+
+console.log(mod(68, 45));
