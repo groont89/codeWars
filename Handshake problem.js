fe3b34ae23c768e54ae67717b62074518e49fb5e
Dmitriy <groont@list.ru>
2022-05-31 19:30:21 +0300

    add Handshake problem

---
+++ Handshake problem.js
@@ -0,0 +1,6 @@
+//Handshake problem
+function getParticipants(handshakes){
+    for(var sumHandShake=0,people=0;sumHandShake<handshakes;sumHandShake+=people++);
+    return people;
+}
+console.log(getParticipants(0));
